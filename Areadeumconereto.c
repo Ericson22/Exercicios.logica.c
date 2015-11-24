7cebcef7af217680bb1e0f2fd2b4066ff838de41
Ericson22 <ericsonguerra22@gmail.com>
2015-11-24 17:16:32 -0200

    Area de um Cone reto
    Cone.

---
+++ Areadeumconereto.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <math.h>
+
+main ( ) {
+
+float vol, A, R, H;
+
+char op
+
+printf("cone, cilindro ou esfera?\n");
+
+scanf("%c", &op);
+
+switch(op) {
+
+case char == cone;
+
+printf ("o raio e a altura");
+
+scanf ("%f%f", &r &H);
+
+vol = 3.14*r*r*(H/3);
+
+a = 3.14*r*sgrt((r*r) + (H*H));
+
+
+printf ("volume = %f e area = %f, vol, A);
+
+break;
+
+case char == cilindro;
+
+printf ("o raio e a atura?");
+
+scanf ("%f%f", &r&H);
+
+vol = 3.14 *r*r*H;
+
+A= 2*3.14*r*H;
+
+printf ("volume = %f e area = %f, vol, A);
+
+break;
+
+case char == esfera;
+
+printf ("o raio e a atura?");
+
+scanf ("%f%f", &r&H);
+
+vol = 3.14 *r*r*r;
+
+A= 2*3.14*r*r;
+
+printf ("volume = %f e area = %f, vol, A);
+
+break;
+
+default;
+
+printf ("opção, invalida. Qual a figura?");
+
+scanf("%c", &op");
+
+break
+
+} 
+
+sistem ("pause");
+
+
+}
